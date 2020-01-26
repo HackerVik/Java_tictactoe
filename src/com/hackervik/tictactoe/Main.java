eb965e0817535f4eb6caa29f08d2f7ad28d4ee84
BihackerViktor <bihacker.viktor@gmail.com>
2020-01-26 14:37:13 +0100

    Main

--- src/com/hackervik/tictactoe/Main.java
+++ src/com/hackervik/tictactoe/Main.java
@@ -1,4 +1,28 @@
 package com.hackervik.tictactoe;
 
+import com.hackervik.tictactoe.model.Board;
+import com.hackervik.tictactoe.model.Player;
+
+import java.util.Random;
+
+import static com.hackervik.tictactoe.service.Tictactoe.*;
+
 public class Main {
+    public static void main(String[] args) {
+        int mode = printMenu();
+        Random random = new Random();
+        Board board = initBoard();
+        Player player = new Player();
+        switch (mode) {
+            case 1:
+                humanToHuman(random, board, player);
+                quitGame("\nIt's a tie!");
+            case 2:
+                humanToAi(random, board, player);
+                quitGame("\nIt's a tie!");
+            case 3:
+                quitGame("Program is exiting...");
+        }
+    }
+
 }
