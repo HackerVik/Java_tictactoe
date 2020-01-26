da705c505507ada3b4e3efed8a4880a374d8b563
BihackerViktor <bihacker.viktor@gmail.com>
2020-01-26 14:36:16 +0100

    Board

--- src/com/hackervik/tictactoe/model/Board.java
+++ src/com/hackervik/tictactoe/model/Board.java
@@ -1,4 +1,13 @@
 package com.hackervik.tictactoe.model;
 
 public class Board {
+    public String getBoard() {
+        return board;
+    }
+
+    public void setBoard(String board) {
+        this.board = board;
+    }
+
+    private String board;
 }
