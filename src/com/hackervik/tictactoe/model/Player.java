35a423c73b25a477d4d7d8cbfe6c8c625c117fc6
BihackerViktor <bihacker.viktor@gmail.com>
2020-01-26 14:36:34 +0100

    Player

--- src/com/hackervik/tictactoe/model/Player.java
+++ src/com/hackervik/tictactoe/model/Player.java
@@ -1,4 +1,13 @@
 package com.hackervik.tictactoe.model;
 
 public class Player {
+    private int player = 0;
+
+    public int getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(int player) {
+        this.player = player;
+    }
 }
